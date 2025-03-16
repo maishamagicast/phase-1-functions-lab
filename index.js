bcf6ce8ca032fbc8138653b39a73c29db5b8a9b3
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-03-16 15:39:26 +0300

    functions in functions

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(x){
+    if (x<42)
+    return ((x-42)*-1);
+    else
+    return x-42 ;
+
+}
+function distanceFromHqInFeet(x){
+    distanceFromHqInBlocks(x);
+    return distanceFromHqInBlocks(x)*264;
+}
+
+function distanceTravelledInFeet(start,end){
+    if (start>end)
+        return ((end-start)*-1)* 264;
+    
+    return(end-start)*264;
+}
+
+function calculatesFarePrice(start, end){
+    let distance =distanceTravelledInFeet(start,end);
+    if (distance <=400)
+        return 0;
+    else if (distance >400 && distance<=2000)
+        return (distance-400)*0.02;
+    else if (distance >2000 && distance <=2500)
+        return 25;
+    else if (distance>=2500)
+        return 'cannot travel that far';
+}
